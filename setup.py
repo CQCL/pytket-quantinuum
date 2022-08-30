37fb675d7f6f5842677984492c56a0b66814e2b6
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2022-08-30 14:08:04 +0100

    Update pytket dependency and tighten test. (#28)

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.5, >= 1.5.2",
+        "pytket == 1.6.0rc0",
         "requests >= 2.2",
         "types-requests",
         "websockets >= 7.0",
