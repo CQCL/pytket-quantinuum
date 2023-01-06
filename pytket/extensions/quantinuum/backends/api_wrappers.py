dcbd0388aeb8d3221b25cff5ca2c946e0314ab27
Yao Tang <yao.tang@cambridgequantum.com>
2023-01-06 12:46:32 +0000

    use asyncio.run if running in worker threads

--- pytket/extensions/quantinuum/backends/api_wrappers.py
+++ pytket/extensions/quantinuum/backends/api_wrappers.py
@@ -372,7 +372,13 @@ class QuantinuumAPI:
 
         if "websocket" in jr:
             # wait for job completion using websocket
-            jr = asyncio.get_event_loop().run_until_complete(self._wait_results(job_id))
+            try:
+                loop = asyncio.get_event_loop()
+                jr = loop.run_until_complete(self._wait_results(job_id))
+            except RuntimeError:
+                # no event loop in thread, call asyncio.run to use a new loop
+                jr = asyncio.run(self._wait_results(job_id))
+
         else:
             # poll for job completion
             jr = self._poll_results(job_id)
