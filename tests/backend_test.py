577f72b62fded38a88dfb41e06d1cc3e0ad42f27
Melf <cqc@melf.de>
2023-04-11 14:43:39 +0100

    add kwargs to cost calculation

--- tests/backend_test.py
+++ tests/backend_test.py
@@ -327,9 +327,11 @@ def test_cost_estimate(
         with pytest.raises(ValueError) as e:
             _ = b.cost(c, n_shots)
         assert "Cannot find syntax checker" in str(e.value)
-        estimate = b.cost(c, n_shots, syntax_checker=f"{b._device_name}-1SC")
+        estimate = b.cost(
+            c, n_shots, syntax_checker=f"{b._device_name}-1SC", no_opt=False
+        )
     else:
-        estimate = b.cost(c, n_shots)
+        estimate = b.cost(c, n_shots, no_opt=False)
     if estimate is None:
         pytest.skip("API is flaky, sometimes returns None unexpectedly.")
     assert isinstance(estimate, float)
@@ -674,6 +676,29 @@ def test_wasm(
     c = b.get_compiled_circuit(c)
     h = b.process_circuits([c], n_shots=10, wasm_file_handler=wasfile)[0]
     assert b.get_result(h)
+
+
+@pytest.mark.skipif(skip_remote_tests, reason=REASON)
+@pytest.mark.parametrize(
+    "authenticated_quum_backend", [{"device_name": "H1-1SC"}], indirect=True
+)
+def test_wasm_costs(
+    authenticated_quum_backend: QuantinuumBackend,
+) -> None:
+    wasfile = WasmFileHandler(str(Path(__file__).parent / "testfile.wasm"))
+    c = Circuit(1)
+    c.name = "test_wasm"
+    a = c.add_c_register("a", 8)
+    c.add_wasm_to_reg("add_one", wasfile, [a], [a])
+
+    b = authenticated_quum_backend
+
+    c = b.get_compiled_circuit(c)
+    costs = b.cost(c, n_shots=10, syntax_checker="H1-1SC", wasm_file_handler=wasfile)
+    if costs is None:
+        pytest.skip("API is flaky, sometimes returns None unexpectedly.")
+    assert isinstance(costs, float)
+    assert costs > 0.0
 
 
 @pytest.mark.skipif(skip_remote_tests, reason=REASON)
