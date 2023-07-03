668ebc52b4561f01e9e7e74de654e4cd998b0f38
sjdilkes <silas.dilkes@cambridgequantum.com>
2023-07-03 10:22:18 +0100

    Update backend_test.py

--- tests/integration/backend_test.py
+++ tests/integration/backend_test.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from base64 import b64encode
+from collections import Counter
 from pathlib import Path
 from typing import cast, Callable, Any  # pylint: disable=unused-import
 import json
@@ -406,7 +407,7 @@ def test_postprocess(
 @pytest.mark.skipif(skip_remote_tests, reason=REASON)
 @pytest.mark.parametrize(
     "authenticated_quum_backend",
-    [{"device_name": name} for name in ALL_SYNTAX_CHECKER_NAMES],
+    [{"device_name": name} for name in ALL_SYNTAX_CHECKER_NAMES],  # type: ignore
     indirect=True,
 )
 def test_leakage_detection(
@@ -425,7 +426,7 @@ def test_leakage_detection(
 
 @given(
     n_shots=strategies.integers(min_value=1, max_value=10),  # type: ignore
-    n_bits=strategies.integers(min_value=0, max_value=10),
+    n_bits=strategies.integers(min_value=0, max_value=10),  # type: ignore
 )
 def test_shots_bits_edgecases(n_shots, n_bits) -> None:
     quantinuum_backend = QuantinuumBackend("H1-1SC", machine_debug=True)
