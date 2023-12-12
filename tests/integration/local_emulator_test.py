53f252a27694c0ff555135970affe17663587ae4
Alec Edgington <alec.edgington@quantinuum.com>
2023-12-12 10:09:17 +0000

    Add test for cost().

--- tests/integration/local_emulator_test.py
+++ tests/integration/local_emulator_test.py
@@ -33,6 +33,7 @@ def test_local_emulator(device_name: str) -> None:
     assert b.is_local_emulator
     c0 = Circuit(2).X(0).CX(0, 1).measure_all()
     c = b.get_compiled_circuit(c0)
+    assert b.cost(c, n_shots=10) == 0.0
     h = b.process_circuit(c, n_shots=10)
     r = b.get_result(h)
     counts = r.get_counts()
