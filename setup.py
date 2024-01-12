db5dd81e40c843e73eb832aefb7013783b03b0b8
Silas Dilkes <36165522+sjdilkes@users.noreply.github.com>
2024-01-12 12:55:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     version=metadata["__extension_version__"],
     author="TKET development team",
     author_email="tket-support@quantinuum.com",
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     project_urls={
         "Documentation": "https://tket.quantinuum.com/extensions/pytket-quantinuum/index.html",
         "Source": "https://github.com/CQCL/pytket-quantinuum",
@@ -43,7 +43,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.23",
+        "pytket ~= 1.24.0rc0",
         "requests >= 2.2",
         "types-requests",
         "websockets >= 7.0",
@@ -54,9 +54,9 @@ setup(
     extras_require={"pecos": ["pytket-pecos >= 0.1.4"]},
     classifiers=[
         "Environment :: Console",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
