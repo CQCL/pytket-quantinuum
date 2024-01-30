a0ae7d3746ea2b6a2efc7843963aa9d337a152c8
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2024-01-30 10:45:00 +0000

    [infra] Make pytket-qir a required dependency of pytket-quantinuum. (#349)

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "pytket ~= 1.24",
+        "pytket-qir ~= 0.4.0",
         "requests >= 2.2",
         "types-requests",
         "websockets >= 7.0",
