1d8f4929153118fe38376e8041d20da9d9e296ee
Megan <megan.l.kohagen@quantinuum.com>
2024-01-30 14:57:08 -0600

    Update Signing in example
    Update logging in example to include MFA / Microsoft option

--- examples/python/Quantinuum_signing_in.py
+++ examples/python/Quantinuum_signing_in.py
@@ -3,13 +3,21 @@
 
 # # Signing into the Quantinuum User Portal
 
-# This notebook contains examples of how to sign in via the Quantinuum API.
+# This notebook contains examples of how to sign in.
 
 # **Requirements:**
-# * You must a verified Quantinuum account
-# * You must have accepted the latest [terms and conditions](https://um.qapi.honeywell.com/static/media/user_terms_and_conditions.46957d35.pdf) which can only be done by logging into the [Quantinuum User Portal](https://um.qapi.quantinuum.com)
 
-# ## Login <a class="anchor" id="native-login"></a>
+# * You must a verified Quantinuum account
+# * You must have accepted the latest [terms and conditions](https://um.qapi.honeywell.com/static/media/user_terms_and_conditions.46957d35.pdf) which can only be done by logging into the [Quantinuum User Portal](https://um.qapi.quantinuum.com).
+
+# **Process:**
+
+# There are two ways to login. The option you choose depends on how you set up your account during registration. If you set up your account by registering your email address directly, use Option 1. If you utilized Microsoft credentials to login, use Option 2.
+
+# 1. [Login](#Login)
+# 2. [Login via Microsoft](#Login-via-Microsoft)
+    
+# ## Login
 
 # If you already have a verified user portal account you will be asked to provide your credentials after initializing the `QuantinuumBackend` interface. If you opted to set up a native account during your user registration we recommend you use this approach.
 
@@ -26,5 +34,36 @@ from pytket.extensions.quantinuum import QuantinuumBackend
 machine = "H1-1E"  # Substitute any Quantinuum target
 backend = QuantinuumBackend(device_name=machine)
 backend.login()
+
+# ## Login via Microsoft
+
+# If you would like to login using a Microsoft account you'll need to set the `provider` flag to `Microsoft` when initializing `QuantinuumBackend`. If you signed up with a Microsoft account during your user registration, you will be required to use this approach. 
+
+# Instead of being prompted for your email and password, you will be prompted with this message:  
+
+# ```
+# To sign in:
+# 1) Open a web browser (using any device)
+# 2) Visit https://microsoft.com/devicelogin
+# 3) Enter code #########
+# 4) Enter your Microsoft credentials
+# ``````
+
+# As the prompt suggests, you'll need to open this link and enter the provided device `code` to complete your authentication with Microsoft. The Quantinuum API will wait (at most 15 minutes) for these steps to be completed.
+
+# Once authenticated, Quantinuum will verify the federated login request and attempt to issue tokens. 
+
+from pytket.extensions.quantinuum import QuantinuumBackend
+
+machine = "H1-1E"  # Substitute any Quantinuum target
+backend = QuantinuumBackend(device_name=machine, 
+                            provider='Microsoft')
+backend.login()
+
+# **Additional Notes**
+
+# * If Multi-factor Authentication (MFA) is enabled on your account you will also be required to approve this login request.
+# * If you don't have access to a web browser you can use another device that does such as a phone or another computer to complete this step.
+# * If you receive these error messages:`Unable to complete federated authentication` or `Provider token is invalid`, please check that you are using the same Microsoft account you set up during your registration.
 
 # <div align="center"> &copy; 2024 by Quantinuum. All Rights Reserved. </div>
