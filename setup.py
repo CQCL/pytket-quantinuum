550f7db81964efecec04b92a7ad866812631f329
Alec Edgington <alec.edgington@quantinuum.com>
2024-02-20 13:30:28 +0000

    Bump pytket and pytket-pecos versions.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.24",
+        "pytket ~= 1.25",
         "pytket-qir ~= 0.5.0",
         "requests >= 2.2",
         "types-requests",
@@ -52,7 +52,7 @@ setup(
         "pyjwt ~= 2.4",
         "msal ~= 1.18",
     ],
-    extras_require={"pecos": ["pytket-pecos ~= 0.1.15"]},
+    extras_require={"pecos": ["pytket-pecos ~= 0.1.16"]},
     classifiers=[
         "Environment :: Console",
         "Programming Language :: Python :: 3.10",
