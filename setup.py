9317eacb5b9b0d097dedfc3eba348169b38d831a
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2024-03-19 18:18:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "pytket ~= 1.26",
-        "pytket-qir ~= 0.8.0",
+        "pytket-qir ~= 0.9.0",
         "requests >= 2.2",
         "types-requests",
         "websockets >= 7.0",
