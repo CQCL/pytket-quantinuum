697af960a736014464ff933cfd09ebbab135f8b8
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2024-03-19 10:25:02 +0100

    remove # fidelities: Dict[str, Any] = {}

--- pytket/extensions/quantinuum/backends/quantinuum.py
+++ pytket/extensions/quantinuum/backends/quantinuum.py
@@ -609,8 +609,6 @@ information not available for emulators (E) or syntax checkers (SC)."
         squash = auto_squash_pass({OpType.PhasedX, OpType.Rz})
         target_2qb_gate = self.compilation_config.target_2qb_gate
         assert target_2qb_gate is not None
-        # use default (perfect fidelities) for supported gates
-        # fidelities: Dict[str, Any] = {}
         if target_2qb_gate == OpType.TK2:
             decomposition_passes = []
         elif target_2qb_gate == OpType.ZZPhase:
