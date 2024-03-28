3f8fe8aa99308aeee010e759a9651ffe6f1bd2be
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2024-03-28 13:17:24 +0000

    Update to pytket-pecos 0.1.22. (#404)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         "msal ~= 1.18",
     ],
     extras_require={
-        "pecos": ["pytket-pecos ~= 0.1.21"],
+        "pecos": ["pytket-pecos ~= 0.1.22"],
         "calendar": ["matplotlib ~= 3.8.3", "pandas ~= 2.2.1"],
     },
     classifiers=[
