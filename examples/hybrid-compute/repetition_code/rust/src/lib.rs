54c58fa28b960836893777533fdc67225018e42e
Irfan Khan <irfan.khan@quantinuum.com>
2024-05-23 20:21:54 -0400

    repetition_code rust project setup

--- examples/hybrid-compute/repetition_code/rust/src/lib.rs
+++ examples/hybrid-compute/repetition_code/rust/src/lib.rs
@@ -1,16 +1,21 @@
 use std::collections::HashMap;
-use std::fmt;
 
 #[no_mangle]
-fn decode3(syn: i32, mut PFU: i32) -> i32  { //takes in a string and returns and a string
+fn init(){
+    // This function can have nothing it in, or load some initial function. 
+    // It is needed when passed via the Quantinuum API to warm up the wasm execution environment. 
+    // It can also be used to set up a global state.
+}
+
+#[no_mangle]
+fn decode3(syn: i32, pfu: i32) -> i32  { //takes in a string and returns and a string
     let mut decoder:HashMap<i32,i32> = HashMap::new();
-    let mut PFU_new = 0;
 
     decoder.insert(1, 1); //001 = 1, if syn = 1 then error on qubit 0
     decoder.insert(3, 2); //010 = 2, if sny = 3 then error on qubit 1
     decoder.insert(2, 4); //100 = 4, if syn = 2 then error on qubit 2
 
-    PFU_new = PFU^decoder[&syn];
+    let pfu_new = pfu ^ decoder[&syn];
 
-    return PFU_new
+    return pfu_new
 }
