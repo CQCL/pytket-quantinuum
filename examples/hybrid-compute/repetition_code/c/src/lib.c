e78392ea3b7d361493a79d575de39bf6f3b21bb8
Irfan Khan <irfan.khan@quantinuum.com>
2024-05-28 19:49:23 -0500

    add c source and clang-built wasm binary

---
+++ examples/hybrid-compute/repetition_code/c/src/lib.c
@@ -0,0 +1,15 @@
+int decode3(int syn, int pfu) {
+    int val;
+    if (syn == 1) {
+        val = 1;
+    } else if (syn == 3) {
+        val = 2;
+    } else if (syn == 2) {
+        val = 4;
+    } else {
+        return 0;
+    }
+
+    int pfu_new = pfu ^ val;
+    return pfu_new;
+}
