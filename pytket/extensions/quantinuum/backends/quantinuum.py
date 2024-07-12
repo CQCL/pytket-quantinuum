5e0a767783fec7a0c31f42ec37ff2accc5d7ccf8
Melf <cqc@melf.de>
2024-07-12 15:33:39 +0100

    clean up

--- pytket/extensions/quantinuum/backends/quantinuum.py
+++ pytket/extensions/quantinuum/backends/quantinuum.py
@@ -639,7 +639,6 @@ class QuantinuumBackend(Backend):
     @property
     def default_two_qubit_gate(self) -> OpType:
         """Returns the default two-qubit gate for the device."""
-        assert self._default_2q_gate in self.two_qubit_gate_set
         return self._default_2q_gate
 
     @property
@@ -660,7 +659,7 @@ class QuantinuumBackend(Backend):
         return bool(info.get_misc("system_type") == "local_emulator")
 
     def rebase_pass(self) -> BasePass:
-        assert self.compilation_config.target_2qb_gate is not None
+        assert self.compilation_config.target_2qb_gate in self.two_qubit_gate_set
         return auto_rebase_pass(
             (self._gate_set - self.two_qubit_gate_set)
             | {self.compilation_config.target_2qb_gate},
