bb5ab02fb7352ceda767d336065fa66eba966727
Melf <cqc@melf.de>
2024-07-12 15:25:20 +0100

    add assert for mypy

--- pytket/extensions/quantinuum/backends/quantinuum.py
+++ pytket/extensions/quantinuum/backends/quantinuum.py
@@ -660,6 +660,7 @@ class QuantinuumBackend(Backend):
         return bool(info.get_misc("system_type") == "local_emulator")
 
     def rebase_pass(self) -> BasePass:
+        assert self.compilation_config.target_2qb_gate is not None
         return auto_rebase_pass(
             (self._gate_set - self.two_qubit_gate_set)
             | {self.compilation_config.target_2qb_gate},
