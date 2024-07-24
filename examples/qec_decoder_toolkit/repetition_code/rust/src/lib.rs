3a13ddb2edd1663196d5218bc2f3bd4a6acf85fb
Irfan Khan <irfan.khan@quantinuum.com>
2024-07-24 11:19:15 +0100

    define unsafe operations in rust decode3 function

--- examples/qec_decoder_toolkit/repetition_code/rust/src/lib.rs
+++ examples/qec_decoder_toolkit/repetition_code/rust/src/lib.rs
@@ -12,15 +12,16 @@ fn init(){
 #[no_mangle]
 fn decode3(syn: i32, pfu: i32) -> i32  { //takes in a string and returns and a string
     let mut decoder:HashMap<i32,i32> = HashMap::new();
-
-    // let syn_new: i32 = SYN_OLD ^ syn;
     decoder.insert(1, 1); //001 = 1, if syn = 1 then error on qubit 0
     decoder.insert(3, 2); //010 = 2, if sny = 3 then error on qubit 1
     decoder.insert(2, 4); //100 = 4, if syn = 2 then error on qubit 2
 
     unsafe {
+        let syn_new: i32 = SYN_OLD ^ syn;
+    }
+
+    unsafe {
         SYN_OLD = syn;
-        println!("SYN_OLD: {}, syn: {}", SYN_OLD, syn)
     }
 
     if syn == 0 {
@@ -31,10 +32,10 @@ fn decode3(syn: i32, pfu: i32) -> i32  { //takes in a string and returns and a s
     }
 }
 
-fn main() -> i32 {
-    let pfu: i32 = decode3(1, 2);
-    return 0;
-}
+// fn main() {
+//     let pfu: i32 = decode3(3, 2);
+//     println!("{}", pfu)
+// }
 
 // #[test]
 // fn test_decode3() {
