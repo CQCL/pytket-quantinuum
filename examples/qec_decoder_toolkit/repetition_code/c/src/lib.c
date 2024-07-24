492336fa36bd26345e59f7c875627e5e24e6ea3e
Irfan Khan <irfan.khan@quantinuum.com>
2024-07-24 11:37:13 +0100

    remove header file from c source

--- examples/qec_decoder_toolkit/repetition_code/c/src/lib.c
+++ examples/qec_decoder_toolkit/repetition_code/c/src/lib.c
@@ -1,4 +1,16 @@
-#include <stdio.h>
+// Copyright 2020-2024 Quantinuum
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 void init() {
     return;
@@ -21,9 +33,3 @@ int decode3(int syn, int pfu) {
     syn_old = syn;
     return val ^ pfu;
 }
-
-// int main() {
-//     int pfu = decode3(1, 2);
-//     printf("%d", pfu);
-//     return 0;
-// }
