1eac19df1b5c6c6f5bd661fd132046e863cce447
Irfan Khan <irfan.khan@quantinuum.com>
2024-09-11 16:31:25 +0100

    surface code rust, cargo.toml and wasm output

---
+++ examples/qec_decoder_toolkit/surface_code/rust/src/lib.rs
@@ -0,0 +1,141 @@
+// Copyright 2020-2024 Quantinuum
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+static mut PFU:i32 = 0; // global variable to track the value of the Pauli Frame Update
+static mut PFU1:i32 = 0; // global variable to track the value of the Pauli Frame Update
+
+
+#[no_mangle]
+fn init(){
+    // This function can have nothing it in, or load some initial function. 
+    // It is needed when passed via the Quantinuum API to warm up the wasm execution environment. 
+    // It can also be used to set up a global state.
+}
+
+
+#[no_mangle]
+fn set_pfu_values(syn: i32) {
+    unsafe{
+        if syn == 1 {
+            PFU = 4; // 000000100
+        }
+        if syn == 2 {
+            PFU = 128; // 010000000
+        }
+        if syn == 4 {
+            PFU = 1; // 000000001
+        }
+        if syn == 8 {
+            PFU = 64; // 001000000
+        }
+        if syn == 3 {
+            PFU = 32; // 000100000
+        }
+        if syn == 6 {
+            PFU = 16; // 000010000
+        }
+        if syn == 12 {
+            PFU = 8; // 000001000
+        }
+        if syn == 5 {
+            PFU = 16; // 000010000
+            PFU1 = 32; // 000001000
+        }
+        if syn == 10 {
+            PFU = 8; // 000001000
+            PFU1 = 16; // 000001000
+        }
+        if syn == 9 {
+            PFU = 4; // 000000100
+            PFU1 = 64; // 001000000
+        }
+        if syn == 7 {
+            PFU = 1; // 000000001
+            PFU1 = 32; // 000100000
+        }
+        if syn == 11 {
+            PFU = 32; // 000100000
+            PFU1 = 64; // 001000000
+        }
+        if syn == 13 {
+            PFU = 8; // 000000100
+            PFU1 = 4; // 000000010
+        }
+        if syn == 14 {
+            PFU = 8; // 000000100
+            PFU1 = 128; // 010000000
+        }
+        if syn == 15 {
+            PFU = 8; // 000000100
+            PFU1 = 32; // 000010000
+        }
+    }
+}
+
+
+#[no_mangle]
+fn get_pfu() -> i32 {
+    unsafe{
+        return PFU;
+    }
+}
+
+#[no_mangle]
+fn get_pfu1() -> i32 {
+    unsafe{
+        return PFU1;
+    }
+}
+
+
+#[no_mangle]
+fn reset_pfu() {
+    unsafe{
+        PFU = 0;
+        PFU1 = 0;
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        reset_pfu();
+        let val: i32 = 15;
+        set_pfu_values(val);
+        assert_eq!(get_pfu(), 8);
+        assert_eq!(get_pfu1(), 32);
+    }
+
+    #[test]
+    fn test2() {
+        reset_pfu();
+        let val: i32 = 4;
+        set_pfu_values(val);
+        assert_eq!(get_pfu(), 1);
+        assert_eq!(get_pfu1(), 0);
+    }
+
+    #[test]
+    fn test3() {
+        set_pfu_values(5);
+        reset_pfu();
+        assert_eq!(get_pfu(), 0);
+        assert_eq!(get_pfu1(), 0);
+    }
+}
